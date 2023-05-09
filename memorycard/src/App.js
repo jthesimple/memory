bbb8c5d520e8a3d8b08875af19390762624d10c9
Julio <julio.bermudez619@gmail.com>
2023-05-09 15:56:35 -0700

    Starting files

---
+++ memorycard/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Images from './components/Gallery'
+
+function App() {
+  return(
+    <>
+  <div>hello</div>
+  <Images></Images>
+  </>
+  )
+
+};
+
+export default App;
+
---
+++ memorycard/src/components/Gallery.js
@@ -0,0 +1,22 @@
+import React from "react";
+import tiger from '../images/tiger.jpeg'
+
+
+
+//function Gallery(){
+
+//};
+
+
+export default function Images(){
+    return (
+        <>
+        <img src={tiger} alt='tiger' style={{width: '120px', height: '120px'}}></img>
+        </>
+    );
+
+};
+
+
+
+
